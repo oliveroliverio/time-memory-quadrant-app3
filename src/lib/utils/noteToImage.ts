28c704b3ba81610da064ba0f4e882edb3e9f226d
oliveroliverio <oliveroliverio@gmail.com>
2024-10-19 12:46:58 -0700

    works now

--- src/lib/utils/noteToImage.ts
+++ src/lib/utils/noteToImage.ts
@@ -5,11 +5,11 @@ const noteToImageMap: { [key: string]: string } = {
     "A": "4_A.png",
     "E": "5_E.png",
     "B": "6_B.png",
-    "Fsh_Gb": "7_Fsh_Gb.png",
-    "Csh_Db": "8_Csh_Db.png",
-    "Gsh_Ab": "9_Gsh_Ab.png",
-    "Dsh_Eb": "10_Dsh_Eb.png",
-    "Ash_Bb": "11_Ash_Bb.png",
+    "F#_Gb": "7_Fsh_Gb.png",
+    "C#_Db": "8_Csh_Db.png",
+    "G#_Ab": "9_Gsh_Ab.png",
+    "D#_Eb": "10_Dsh_Eb.png",
+    "A#_Bb": "11_Ash_Bb.png",
     "F": "12_F.png"
   };
 
